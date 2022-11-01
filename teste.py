bfbbeca9125aa0c422a9c9dd64edfbf9ed824ded
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2022-11-01 16:47:07 -0300

    Acrescentei Aula 12 sobre Beautiful Soup.

---
+++ teste.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.globo.com"
+r = requests.get (url)
+html = r.text
+soup = BeautifulSoup(html, 'html.parser')
+
+# Deixa bonito o html
+print(soup.prettify())
+
+# Pega o title do html
+print (soup.title)
+
+# Pesquisa por todos "a", pode colocar na lista e trabalhar com ela
+print (soup.find_all('a'))
+
+# Pegando os links do "a"
+for link in soup.find_all('a'):
+    print(link.get('href'))
