489ffb135cb69672141678e7e2b8fca06802edde
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2022-11-01 16:52:03 -0300

    Acrescentei Aula 12 de Beautiful Soup.

--- teste.py
+++ teste.py
@@ -15,6 +15,9 @@ print (soup.title)
 # Pesquisa por todos "a", pode colocar na lista e trabalhar com ela
 print (soup.find_all('a'))
 
+# Pesquisa por uma id expecifica
+print (soup.find(id="globalWebdepsScript"))
+
 # Pegando os links do "a"
 for link in soup.find_all('a'):
     print(link.get('href'))
