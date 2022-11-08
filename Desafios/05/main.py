0c99dfa0c4b078e239ea9c7842df3f58dee627bb
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2022-11-08 10:59:01 -0300

    Fazendo os menus no desafio 05.

--- Desafios/05/main.py
+++ Desafios/05/main.py
@@ -1,22 +1,58 @@
-🔥 O Desafio de Hoje
-Hoje você precisa fazer a parte 1 do programa: Negociador de Moedas
-O programa deve fazer um scrapping no site do IBAN e capturar a listagem de países e suas respectivas moedas.
-Após isso deve apresentar a lista de países para o usuário e permitir que o usuário consulte qual é o código da moeda do país selecionado.
-Veja no vídeo da aula como o programa deve funcionar.
-As outras funcionalidades (parte 2) faremos nos próximos dias.
+import requests
+from bs4 import BeautifulSoup
 
-⚱️ Dicas de Ouro
-É deste site que você deve pegar os países e suas moedas: https://www.iban.com/currency-codes
-Faça o Scraping usando o Requests e Beautiful Soup (como ensinado no conteúdo do dia).
-No site do IBAN, no HTML a listagem é feita em uma tabela (table).
-Crie uma lista [] para armazenar os países e seus códigos de moeda.
-Cada país pode ser um dicionário dentro da lista, contendo as chaves 'nome' e 'código da moeda'. (uma dica de organização 🙂 )
-Alguns países não possuem moeda universal (No universal currency), não adicione esses países na lista.
-A input do usuário deve ser apenas números e não pode ser maior do que a listagem do menu (opções do menu).
-Use try: / except: quando converter a input do usuário para int. (pode ajudar).
-Após o usuário selecionar um país, informe o código da moeda daquele país.
+def menu():
+    choice = str(input("Precisa verificar mais algum site? s/n ")).lower()
+    if choice == "s" or choice =="n":
+        if choice == "n":
+            print("Programa Encerrado!")
+            return
+        elif choice == "s":
+            main()
+    else:
+        print("Opção inválida.")
+        menu()
 
-🥇 Conteúdo que pode te ajudar muito:
-https://stackoverflow.com/questions/522563/accessing-the-index-in-for-loops
 
-Link do formulário de entrega: https://form.typeform.com/to/oig2ytw1
+def main ():
+    print ("Bem-vindo ao Negociador de Moedas 1.0")
+    print ("Escolha pelo número da lista o país que deseja consultar o código da moeda")
+
+    url = "https://www.iban.com/currency-codes"
+    r = requests.get (url)
+    html = r.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    cards = soup.find_all("tbody")
+
+    for card in cards:
+        print (card.find("td"))
+        break
+    
+    menu()
+main()
+
+# print (cards)
+
+# <tr>
+# <td>AFGHANISTAN</td>
+# <td>Afghani</td>
+# <td>AFN</td>
+# <td>971</td>
+# </tr>
+
+
+# No site do IBAN, no HTML a listagem é feita em uma tabela (table).
+# Crie uma lista [] para armazenar os países e seus códigos de moeda.
+# Cada país pode ser um dicionário dentro da lista, contendo as chaves 'nome' e 'código da moeda'.
+# Alguns países não possuem moeda universal (No universal currency), não adicione esses países na lista.
+# A input do usuário deve ser apenas números e não pode ser maior do que a listagem do menu (opções do menu).
+# Validar para quando digitar letras, apenas aceitar números (print = "Isso não é um número")
+# Validar valores maiores que o tamanho da lista (print = Não existe, escolha uma opção da lista:)
+# Use try: / except: quando converter a input do usuário para int.
+# Após o usuário selecionar um país, informe o código da moeda daquele país (print = Você escolheu [país] e o código da moeda é [código])
+
+# 🥇 Conteúdo que pode te ajudar muito:
+# https://stackoverflow.com/questions/522563/accessing-the-index-in-for-loops
+
+# Link do formulário de entrega: https://form.typeform.com/to/oig2ytw1
