5e67d275a6839e8c99e1da90a4e90225a402aac7
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2022-11-09 16:54:47 -0300

    Consegui obter informações da url, transformei as informações em uma lista.

--- Desafios/05/main.py
+++ Desafios/05/main.py
@@ -1,9 +1,17 @@
 import requests, os
 from bs4 import BeautifulSoup
 
+# Criei essa função para limpar a string com os caracteres especiais
+def limpando (texto):
+    remover = "[<td>]/ " 
+    for remover in remover:
+        texto = texto.replace(remover, "")
+    return texto
+
+# Função menu
 def menu():
     os.system('cls' if os.name == 'nt' else 'clear')
-    choice = str(input("Precisa verificar mais algum site? s/n ")).lower()
+    choice = str(input("Precisa verificar mais algum site? s/n: ")).lower()
     if choice == "s" or choice =="n":
         if choice == "n":
             print("Programa Encerrado!")
@@ -11,40 +19,32 @@ def menu():
         elif choice == "s":
             main()
     else:
-        print("Opção inválida.")
+        input ("Opção inválida!\nTecle enter para digitar novamente!")
         menu()
 
-
+# Função main
 def main ():
+    os.system('cls' if os.name == 'nt' else 'clear')
     # Criando o Beautiful Soup
     url = "https://www.iban.com/currency-codes"
-    r = requests.get (url)
-    html = r.text
-    soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    
+    # Coletando informações na URL
+    cards = soup.find_all("tr")
+    update = str (soup.find("p").next_sibling).replace("\n", "")
 
     print ("Bem-vindo ao Negociador de Moedas 1.0")
-    print ()
-    print ("Escolha pelo número da lista o país que deseja consultar o código da moeda")
-
-    cards = soup.find_all("tbody")
-    for card in cards:
-        print (card.find("td"))
-        break
+    print (update + " da " + url)
+    input ("Tecle enter para iniciar o programa!")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    resposta = input("Escolha pelo número da lista o país que deseja consultar o código da moeda: ")
 
     menu()
 main()
 
-# print (cards)
+# view-source:https://www.iban.com/currency-codes
+# Link do formulário de entrega: https://form.typeform.com/to/oig2ytw1
 
-# <tr>
-# <td>AFGHANISTAN</td>
-# <td>Afghani</td>
-# <td>AFN</td>
-# <td>971</td>
-# </tr>
-
-
-# No site do IBAN, no HTML a listagem é feita em uma tabela (table).
 # Crie uma lista [] para armazenar os países e seus códigos de moeda.
 # Cada país pode ser um dicionário dentro da lista, contendo as chaves 'nome' e 'código da moeda'.
 # Alguns países não possuem moeda universal (No universal currency), não adicione esses países na lista.
@@ -53,8 +53,3 @@ main()
 # Validar valores maiores que o tamanho da lista (print = Não existe, escolha uma opção da lista:)
 # Use try: / except: quando converter a input do usuário para int.
 # Após o usuário selecionar um país, informe o código da moeda daquele país (print = Você escolheu [país] e o código da moeda é [código])
-
-# 🥇 Conteúdo que pode te ajudar muito:
-# https://stackoverflow.com/questions/522563/accessing-the-index-in-for-loops
-
-# Link do formulário de entrega: https://form.typeform.com/to/oig2ytw1
--- teste.py
+++ teste.py
@@ -1,35 +1,26 @@
-# view-source:https://www.iban.com/currency-codes
-
 import requests
 from bs4 import BeautifulSoup
 
-# Criando a Lista
-#moedas = []
+def limpandoString (texto):
+    caracteres = "[<td>]/ " 
+    for caracteres in caracteres:
+        texto = texto.replace(caracteres, "")
+    return texto
 
+def criarLista (sopa):
+    for card in sopa:
+        sopa = limpandoString(str(card.find_all("td"))).split(",")
+        print (sopa)
+
+# view-source:https://www.iban.com/currency-codes
+
+moedas = []
 url = "https://www.iban.com/currency-codes"
-r = requests.get (url)
-html = r.text
-soup = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(requests.get(url).text, "html.parser")
+cards = soup.find_all("tr")
+criarLista (cards)
 
-cards = soup.find_all("tbody")
-#print (cards)
-for card in cards:
-#   dicionario = {"country": "cidade", "currency": "moeda", "code": "código", "number": "numero"}
-    print (card.find("tr").get_text())
-#   moedas.append (dicionario)
-
-# PRIMEIRO DA LISTA
-# <tr>
-# <td>AFGHANISTAN</td>
-# <td>Afghani</td>
-# <td>AFN</td>
-# <td>971</td>
-# </tr>
-
-# ULTIMO DA LISTA
-# <tr>
-# <td>ZIMBABWE</td>
-# <td>Zimbabwe Dollar</td>
-# <td>ZWL</td>
-# <td>932</td>
-# </tr>
+# PRIMEIRO DA LISTA - <td>AFGHANISTAN</td>
+# ULTIMO DA LISTA - <td>ZIMBABWE</td>
+# dicionario = {"country": "cidade", "currency": "moeda", "code": "código", "number": "numero"}
+# moedas.append (dicionario)
