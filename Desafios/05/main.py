ce5936b7d94161c4b2535249dbb6039fd0d1bc2d
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2022-11-10 16:55:55 -0300

    Resolvi o problema de criar o dicionário dentro da lista, preciso apenas remover o lixo que existe na lista.

--- Desafios/05/main.py
+++ Desafios/05/main.py
@@ -1,57 +1,58 @@
 import requests, os
 from bs4 import BeautifulSoup
 
-def menu():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    choice = str(input("Precisa verificar mais algum site? s/n: ")).lower()
-    if choice == "s" or choice =="n":
-        if choice == "n":
-            print("Programa Encerrado!")
-            return
-        elif choice == "s":
-            main()
-    else:
-        input ("Opção inválida!\nTecle enter para digitar novamente!")
-        menu()
-
 def limpandoString (texto):
     caracteres = "[<td>]/ " 
     for caracteres in caracteres:
         texto = texto.replace(caracteres, "")
     return texto
 
-def criarDicionario (sopa, lista):
-    for card in sopa:
-        sopa = limpandoString(str(card.find_all("td"))).split(',')
-        print (sopa[1])
-        
+def criarLista (sopa, moedas):
+    for texto in sopa:
+        nome = ["cidade", "moeda", "código", "numero"]
+        sopa = limpandoString(str(texto.find_all("td"))).split(",")
+
+        dicionario = dict()
+        for chave, valor in zip(nome, sopa):
+            # remover cidade com
+            if chave == "cidade" or chave == "código":
+                dicionario [chave] = valor
+        print (dicionario)
+        input ("espere")
+        moedas.append (dicionario)
 
 
-def main ():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    url = "https://www.iban.com/currency-codes"
-    soup = BeautifulSoup(requests.get(url).text, "html.parser")
-    update = str (soup.find("p").next_sibling).replace("\n", "")
-    cards = soup.find_all("tr")
-    lista = []
-    criarDicionario (cards, lista)
-    print (lista)    
-    print ("Bem-vindo ao Negociador de Moedas 1.0")
-    print (update + " da " + url)
-    input ("Tecle enter para iniciar o programa!")
-    os.system('cls' if os.name == 'nt' else 'clear')
-    resposta = input("Escolha pelo número da lista o país que deseja consultar o código da moeda: ")
-    #menu()
-main()
+# Programa principal
+moedas = []      
+url = "https://www.iban.com/currency-codes"
+soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+update = str (soup.find("p").next_sibling).replace("\n", "")
+cards = soup.find_all("tr")
+
+# Menu de Boas Vindas
+print ("Bem-vindo ao Negociador de Moedas 1.0")
+print (update + " da " + url)
+input ("Tecle enter para iniciar o programa!")
+os.system('cls' if os.name == 'nt' else 'clear')
+
+criarLista(cards, moedas)
+
+    
 
 # view-source:https://www.iban.com/currency-codes
+# ULTIMO DA LISTA - <td>ZIMBABWE</td>
+# dicionario = {"country": "cidade", "currency": "moeda", "code": "código", "number": "numero"}
+
 # Link do formulário de entrega: https://form.typeform.com/to/oig2ytw1
 
-# Crie uma lista [] para armazenar os países e seus códigos de moeda.
-# Cada país pode ser um dicionário dentro da lista, contendo as chaves 'nome' e 'código da moeda'.
+
+
 # Alguns países não possuem moeda universal (No universal currency), não adicione esses países na lista.
+
+# Imprimir a lista => print(#[número] - [Nome do País])
 # A input do usuário deve ser apenas números e não pode ser maior do que a listagem do menu (opções do menu).
-# Validar para quando digitar letras, apenas aceitar números (print = "Isso não é um número")
-# Validar valores maiores que o tamanho da lista (print = Não existe, escolha uma opção da lista:)
+# Validar para quando digitar letras, apenas aceitar números => print("Isso não é um número")
+# Validar valores maiores que o tamanho da lista => print("Não existe, escolha uma opção da lista!")
 # Use try: / except: quando converter a input do usuário para int.
-# Após o usuário selecionar um país, informe o código da moeda daquele país (print = Você escolheu [país] e o código da moeda é [código])
+# Após o usuário selecionar um país, informe o código da moeda daquele país => print (Você escolheu [país] e o código da moeda é [código])
--- teste.py
+++ teste.py
@@ -7,20 +7,20 @@ def limpandoString (texto):
         texto = texto.replace(caracteres, "")
     return texto
 
-def criarLista (sopa):
-    for card in sopa:
-        sopa = limpandoString(str(card.find_all("td"))).split(",")
-        print (sopa)
-
-# view-source:https://www.iban.com/currency-codes
-
-moedas = []
-url = "https://www.iban.com/currency-codes"
-soup = BeautifulSoup(requests.get(url).text, "html.parser")
-cards = soup.find_all("tr")
-criarLista (cards)
-
-# PRIMEIRO DA LISTA - <td>AFGHANISTAN</td>
-# ULTIMO DA LISTA - <td>ZIMBABWE</td>
-# dicionario = {"country": "cidade", "currency": "moeda", "code": "código", "number": "numero"}
-# moedas.append (dicionario)
+def criarLista (sopa, moedas):
+    for texto in sopa:
+        nome = ["cidade", "moeda", "código", "numero"]
+        sopa = limpandoString(str(texto.find_all("td"))).split(",")
+        dicionario = dict()
+        for chave, valor in zip(nome, sopa):
+            dicionario [chave] = valor
+        moedas.append (dicionario)
+        
+def main ():
+    url = "https://www.iban.com/currency-codes"
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    update = str (soup.find("p").next_sibling).replace("\n", "")
+    cards = soup.find_all("tr")
+    moedas = []
+    criarLista(cards, moedas)
+    print (moedas)
