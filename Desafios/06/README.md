f4dd6c04314ab6902fd4aae5bf37678773008700
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2022-11-18 17:12:11 -0300

    Começando o Desafio 06.

---
+++ Desafios/06/README.md
@@ -0,0 +1,36 @@
+# Negociador de Moedas 1.0
+Web Scrapping on the IBAN website and capturing a list of countries and their reference currencies.
+
+## Source Code
+You can also download [Negociador de Moedas 1.0](https://github.com/joeywheelersam/maratona-python/tree/master/Desafios/05) directly from its [Git repository](https://github.com/joeywheelersam/maratona-python.git):
+
+```
+git clone https://github.com/joeywheelersam/maratona-python.git
+```
+
+## Dependencies
+[Negociador de Moedas 1.0](https://github.com/joeywheelersam/maratona-python/tree/master/Desafios/05) works out of the box with [Python 3.10](https://www.python.org/downloads/release/python-3100/) on any platform and requires the following packages:
+- [Requests 2.28.1+](https://pypi.org/project/requests/)
+- [Beautiful Soup 4.11.1+](https://pypi.org/project/beautifulsoup4/)
+
+## Installation
+How to install dependencies:
+```
+pip install requests
+```
+```
+pip install beautifulsoup4
+```
+
+## Usage
+How to run the program:
+```
+python main.py
+```
+
+## License
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+[Negociador de Moedas 1.0](https://github.com/joeywheelersam/maratona-python/tree/master/Desafios/05) is free software, keeping the picture can USE AND ABUSE 
---
+++ Desafios/06/main.py
@@ -0,0 +1,76 @@
+import requests, os
+from bs4 import BeautifulSoup
+from babel.numbers import format_currency
+
+# Função que formata o texto da URL
+def formatacao (texto):
+    caracteres = "[<td>]/ " 
+    for caracteres in caracteres:
+        texto = texto.replace(caracteres, "")
+    return texto
+
+# Função que cria o database com todos os dados necessários da URL
+def database (sopa):
+    for texto in sopa:
+        nome = ["cidade", "moeda", "código", "numero"]
+        sopa = formatacao(str(texto.find_all("td"))).split(",")
+        dicionario = dict()
+        universalCurrency = "Nouniversalcurrency" in sopa
+        if universalCurrency == False:
+            for chave, valor in zip(nome, sopa):
+                if chave == "cidade" or chave == "código":
+                    dicionario [chave] = valor
+        else:
+            continue
+        moedas.append (dicionario)
+
+# Programa principal
+moedas = []      
+
+# Requisição a URL
+url = "https://www.iban.com/currency-codes"
+soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+# Capturando as informações necessárias da URL
+update = str (soup.find("p").next_sibling).replace("\n", "")
+tabela = soup.find_all("tr")
+del tabela[0] # Deletei o primeiro item da lista porque o valor chegou vazio.
+
+# Criando o database 
+database(tabela)
+
+# Menu informando o nome do aplicativo
+print ("Bem-vindo ao Negociador de Moedas 1.0")
+print (update + " da " + url)
+print ("Escolha pelo número os dois países que deseja negociar as moedas.")
+input ("Tecle enter para iniciar o programa!")
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Lista todas as moedas cadastradas
+for numero, lista in enumerate(moedas):
+        print (f"#{numero} - {lista['cidade']}")
+
+# Validação da solicitação do usuário
+while True:
+    try:
+        resposta = int(input ("#: "))
+        if (resposta <= len(moedas)):
+            print ("Você escolheu o "+ moedas[resposta].get("cidade"))
+            print ("O código da moeda é "+ moedas[resposta].get("código"))
+            break
+        else:
+            print ("Não existe, escolha uma opção da lista!")
+            continue
+    except ValueError:
+        print("Isso não é um número")
+
+
+
+# Link do formulário de entrega: https://form.typeform.com/to/FplJEfwQ​
+# Para isso vamos fazer um scrapping na Wise informando os códigos e quantidade via URL. (https://wise.com/gb/currency-converter/brl-to-usd-rate?amount=50)
+# O programa deve permitir que o usuário selecione 2 países da lista (origem e destino) e informe a quantidade que deseja converter entre moedas.
+# Envie os códigos das moedas dos países selecionados + quantia de conversão para o site da TransferWise via URL: Exemplo: https://wise.com/gb/currency-converter/brl-to-usd-rate?amount=5
+# Veja o 'brl' para 'usd' na url e também o amout=quantia...
+# Faça o Scrapping na TransferWise com o Requests e Beautiful Soup
+# Talvez você não consiga pegar o valor já convertido da Wise, seja inteligente e faça um cálculo simples pegando um outro valor chave lá na página.
+# Para retornar pro usuário o valor formatado você pode usar o Babel.numbers http://babel.pocoo.org/en/latest/api/numbers.html Exemplo de uso da função format_currency:
